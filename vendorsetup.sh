ea785ca8c8d734d26bede9f2f06161b3f1dd3f51
lahaina <nattolecats@gmail.com>
2024-04-14 13:24:07 +0900

    sunfish: Always update KSU latest version

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 rm -rf vendor/gms/product/packages/privileged_apps/DeviceIntelligenceNetworkPrebuilt
+
+# KernelSU
+KSU=kernel/google/msm-4.14/KernelSU
+rm -rf $KSU && git clone https://github.com/tiann/KernelSU $KSU
